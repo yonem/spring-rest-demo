6fba59dbed4ea30bfdec21f6fe7647c6a0303632
yonem <yonem321@gmail.com>
2025-08-03 09:21:03 +0900

    feat: Member登録機能を追加する #44

---
+++ src/test/java/jp/ne/yonem/restful/demo/mapper/MemberMapperTest.java
@@ -0,0 +1,22 @@
+package jp.ne.yonem.restful.demo.mapper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import jp.ne.yonem.restful.demo.entity.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
+
+@MybatisTest
+@Sql("classpath:sql/MemberMapperTest.sql")
+class MemberMapperTest {
+  @Autowired private MemberMapper sut;
+
+  @Test
+  @DisplayName("Member insert")
+  void test1() {
+    assertEquals(1, sut.insert(new Member(null, 1, "member1")));
+  }
+}
---
+++ src/test/resources/sql/MemberMapperTest.sql
@@ -0,0 +1,2 @@
+INSERT INTO team (id, name) VALUES (1, 'Team A');
+INSERT INTO member (team_id, name) VALUES (1, 'Hoge');
