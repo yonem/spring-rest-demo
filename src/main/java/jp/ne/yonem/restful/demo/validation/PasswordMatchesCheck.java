0a40012535c217ea902658579472fbb33ee9fb39
yonem <yonem321@gmail.com>
2025-09-01 21:56:16 +0900

    feat: パスワードマッチバリデーションを実装する #72

---
+++ src/main/java/jp/ne/yonem/restful/demo/validation/PasswordMatchesCheck.java
@@ -0,0 +1,22 @@
+package jp.ne.yonem.restful.demo.validation;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Documented
+@Constraint(validatedBy = PasswordMatchesValidator.class)
+@Target({TYPE})
+@Retention(RUNTIME)
+public @interface PasswordMatchesCheck {
+  String message() default "Passwords don't match";
+
+  Class<?>[] groups() default {};
+
+  Class<? extends Payload>[] payload() default {};
+}
