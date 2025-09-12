8f7503f06ee84f2e0f2d37fb3af351789ebe7b11
yonem <yonem321@gmail.com>
2025-09-12 20:05:40 +0900

    feat: ダウンロードファイルをResponseEntityでreturnする #80

--- src/main/java/jp/ne/yonem/restful/demo/controller/CsvDownloadController.java
+++ src/main/java/jp/ne/yonem/restful/demo/controller/CsvDownloadController.java
@@ -1,28 +1,22 @@
 package jp.ne.yonem.restful.demo.controller;
 
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import jp.ne.yonem.restful.demo.service.UserService;
+import jp.ne.yonem.restful.demo.service.UserCsvService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api")
 @RestController
+@RequiredArgsConstructor
 public class CsvDownloadController {
-  private final UserService userService;
-
-  public CsvDownloadController(UserService userService) {
-    this.userService = userService;
-  }
+  private final UserCsvService userService;
 
   @GetMapping("/download/users.csv")
-  public void downloadCsv(HttpServletResponse response) throws IOException {
-    response.setContentType("text/csv; charset=UTF-8");
-    response.setHeader("Content-Disposition", "attachment; filename=\"users.csv\"");
-
-    try (var writer = response.getWriter()) {
-      userService.execute(writer);
-    }
+  public ResponseEntity<?> downloadCsv() throws IOException {
+    var csv = userService.execute();
+    return ResponseEntity.ok().headers(csv.getHeader()).body(csv.getFile());
   }
 }
--- src/test/java/jp/ne/yonem/restful/demo/service/UserServiceTest.java
+++ src/test/java/jp/ne/yonem/restful/demo/service/UserServiceTest.java
@@ -2,8 +2,7 @@ package jp.ne.yonem.restful.demo.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.io.IOException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,17 +11,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
-  @InjectMocks private UserService sut;
+  @InjectMocks private UserCsvService sut;
 
   @Test
   @DisplayName("ユーザーリストがCSV形式で正しく出力されること")
-  void test01() throws Exception {
-    var stringWriter = new StringWriter();
-    var printWriter = new PrintWriter(stringWriter);
-    var exp =
-        String.join(System.lineSeparator(), "name,age", "Alice,25", "Bob,30", "Charlie,35", "");
-
-    sut.execute(printWriter);
-    assertEquals(exp.strip(), stringWriter.toString().strip());
+  void test01() throws IOException {
+    var exp = String.join(System.lineSeparator(), "name,age", "Alice,25", "Bob,30", "Charlie,35");
+    var act = sut.execute();
+    var file = new String(act.getFile());
+    assertEquals(exp, file);
   }
 }
