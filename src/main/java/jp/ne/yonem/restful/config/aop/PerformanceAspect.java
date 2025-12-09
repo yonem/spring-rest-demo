b74c65c66464d3eb9ede0388cd1ea4b57628b9c7
yonem <yonem321@gmail.com>
2025-12-10 07:57:05 +0900

    fix: AOPのパス修正とControllerログの追加 #154

--- src/main/java/jp/ne/yonem/restful/config/aop/PerformanceAspect.java
+++ src/main/java/jp/ne/yonem/restful/config/aop/PerformanceAspect.java
@@ -1,9 +1,11 @@
 package jp.ne.yonem.restful.config.aop;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -11,15 +13,23 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class PerformanceAspect {
 
-  @Around("execution(* jp.ne.yonem.restful.demo.service.*.*(..))")
+  @Around("execution(* jp.ne.yonem.restful.application.*.*(..))")
   public Object measureTime(ProceedingJoinPoint joinPoint) throws Throwable {
     var startTime = System.currentTimeMillis();
-    var result = joinPoint.proceed();
+    var result = joinPoint.proceed(); // 実際のメソッドが実行される
     var endTime = System.currentTimeMillis();
     var executionTime = endTime - startTime;
     var className = joinPoint.getTarget().getClass().getSimpleName();
     var methodName = joinPoint.getSignature().getName();
     log.info("⏳ {}.{} の実行時間: {} ms", className, methodName, executionTime);
     return result;
+  }
+
+  @Before("execution(* jp.ne.yonem.restful.presentation.controller.*.*(..))")
+  public void logRequestStart(JoinPoint joinPoint) {
+    var className = joinPoint.getTarget().getClass().getSimpleName();
+    var methodName = joinPoint.getSignature().getName();
+    var args = joinPoint.getArgs();
+    log.info("▶️ Request Start: {}.{} called with arguments: {}", className, methodName, args);
   }
 }
