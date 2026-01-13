6deca23c44a75b79be827014d21dab63fc5dc0d1
yonem <yonem321@gmail.com>
2026-01-14 08:10:07 +0900

    feat: モダンJavaへの変換レッスン2 #200

--- src/main/java/jp/ne/yonem/restful/infrastructure/lesson/AverageAgeOfAdmins.java
+++ src/main/java/jp/ne/yonem/restful/infrastructure/lesson/AverageAgeOfAdmins.java
@@ -23,4 +23,24 @@ public class AverageAgeOfAdmins {
     }
     return (double) totalAge / adminCount;
   }
+
+  public double modern(List<User> users) {
+    var totalAge = users.stream().reduce(0.0, (age, user) -> age + user.age(), Double::sum);
+    var adminCount = users.stream().filter(user -> "ADMIN".equals(user.role())).count();
+
+    if (adminCount == 0) {
+      return 0.0;
+    }
+    return totalAge / adminCount;
+  }
+
+  public double modern2(List<User> users) {
+
+    // null安全の考慮（実務では Optional.ofNullable(users).stream()... とすることも多い）
+    return users.stream()
+        .filter(user -> "ADMIN".equals(user.role())) // ADMINだけに絞り込む
+        .mapToInt(User::age) // age（int）のストリームに変換
+        .average() // 平均を計算（結果は OptionalDouble）
+        .orElse(0.0); // ADMINがいない場合は 0.0 を返す
+  }
 }
