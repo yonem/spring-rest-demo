2073b1b804759bb692a62e98d4fe1d629e03d286
yonem <yonem321@gmail.com>
2026-01-18 01:32:55 +0900

    feat: モダンJavaへの変換レッスン4 #208

--- src/main/java/jp/ne/yonem/restful/infrastructure/lesson/SalarySumByDept.java
+++ src/main/java/jp/ne/yonem/restful/infrastructure/lesson/SalarySumByDept.java
@@ -3,11 +3,12 @@ package jp.ne.yonem.restful.infrastructure.lesson;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class SalarySumByDept {
   public record Employee(String department, int salary) {}
 
-  public Map<String, Integer> getSalarySumByDept(List<Employee> employees) {
+  public Map<String, Integer> legacy(List<Employee> employees) {
     Map<String, Integer> result = new HashMap<>();
     for (Employee e : employees) {
       String dept = e.department();
@@ -19,5 +20,13 @@ public class SalarySumByDept {
       result.put(dept, result.get(dept) + salary);
     }
     return result;
+  }
+
+  public Map<String, Integer> modern(List<SalarySumByDept.Employee> employees) {
+    return employees.stream()
+        .collect(
+            Collectors.groupingBy(
+                SalarySumByDept.Employee::department,
+                Collectors.summingInt(SalarySumByDept.Employee::salary)));
   }
 }
