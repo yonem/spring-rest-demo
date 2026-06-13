774120a4bf1204ea62b5b295cba7132d50adbff0
yonem <yonem321@gmail.com>
2026-06-13 13:51:51 +0900

    feat: モダンJavaへの変換レッスン29 #433

--- src/main/java/jp/ne/yonem/restful/infrastructure/lesson/factorymethod/FactoryProduct.java
+++ src/main/java/jp/ne/yonem/restful/infrastructure/lesson/factorymethod/FactoryProduct.java
@@ -1,6 +1,6 @@
 package jp.ne.yonem.restful.infrastructure.lesson.factorymethod;
 
 /** 送信対象となるメッセージを表す製品インターフェースです。 */
-public interface FactoryProduct {
+public sealed interface FactoryProduct permits SlackFactoryProduct, EmailFactoryProduct {
   String getContent();
 }
